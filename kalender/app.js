6ea67ac0d01f8c860653712f070849cfda761f9a
EnQiridion <166743755+EnQiridion@users.noreply.github.com>
2025-12-15 09:28:29 +0100

    Update app.js

--- kalender/app.js
+++ kalender/app.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-const eventRoutes = require('./routes/eventRoutes');
+const bodyParser = require('body-parser');
+const budgetRoute = require('./routes/budgetRoute');
+const transactionRoute = require('./routes/transactionRoute');
+const userRoute = require('./routes/userRoute');
+const { logger } = require('./middlewares/logger');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/budgetApp');
 
-app.use('/api/events', eventRoutes);
+app.use(express.json()); app.use(logger);
+app.use('/api', budgetRoute);
+app.use('/api', transactionRoute);
+app.use('/api', userRoute);
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
-});
+app.listen(3000, () => console.log('Server kører på port 3000'));
 
-mongoose.connect('mongodb://localhost:27017/kalender');
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
